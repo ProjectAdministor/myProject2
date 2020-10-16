ceae05c8eca709c7e0be1da2a3ed33e77db33ce4
khb3025 <60993609+khb3025@users.noreply.github.com>
2020-10-16 22:50:28 +0900

    leader계정에서 member계정에서 commit and push 후에 commit and push!

--- naverLoginSpring/src/main/java/com/itwill/naver/login/Example.java
+++ naverLoginSpring/src/main/java/com/itwill/naver/login/Example.java
@@ -4,6 +4,9 @@ public class Example {
 	public void leaderAccountMadeFunc() {
 		System.out.println("github Leader 계정에서 작성한 메소드입니다.");
 	}
+	public void leaderAccountMadeFunc2() {
+		System.out.println("github Leader 계정에서 작성한 두번째 메소드입니다.");
+	}
 
 	
 }
