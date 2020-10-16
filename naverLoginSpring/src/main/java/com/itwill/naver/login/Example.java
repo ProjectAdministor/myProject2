f70f4adf79b6c9866c2b6e76986d274e3b72d017
khb3025 <60993609+khb3025@users.noreply.github.com>
2020-10-16 22:25:46 +0900

    member 계정에서 파일 commit and push 후leader 게정에서

--- naverLoginSpring/src/main/java/com/itwill/naver/login/Example.java
+++ naverLoginSpring/src/main/java/com/itwill/naver/login/Example.java
@@ -4,4 +4,9 @@ public class Example {
 	public void leaderAccountMadeFunc() {
 		System.out.println("github Leader 계정에서 작성한 메소드입니다.");
 	}
+	
+	public void leaderAccountMadeFunc2() {
+		System.out.println("github Leader 계정에서 두번째로 작성한 메소드입니다.");
+	}
+	
 }
