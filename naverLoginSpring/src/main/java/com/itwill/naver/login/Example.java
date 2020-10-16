7448ebcd208eb1fcf0d8e35ff216a2e5529eb893
khb3025 <60993609+khb3025@users.noreply.github.com>
2020-10-16 22:40:29 +0900

    수정후 commit!

--- naverLoginSpring/src/main/java/com/itwill/naver/login/Example.java
+++ naverLoginSpring/src/main/java/com/itwill/naver/login/Example.java
@@ -9,5 +9,7 @@ public class Example {
 		System.out.println("github Leader 계정에서 두번째로 작성한 메소드입니다.");
 	}
 	
-	
+	public void memberAccountMadeFunc() {
+		System.out.println("member 계정에서 작성한 메소드");
+	}
 }
