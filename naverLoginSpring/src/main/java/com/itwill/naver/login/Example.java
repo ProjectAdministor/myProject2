2598b7f40fb8e9568aa3a42aaf9299f2b43086d2
khb3025 <60993609+khb3025@users.noreply.github.com>
2020-11-03 21:37:26 +0900

    팀원계정에서 삭제한 메소드는 그대로, memberFunc() 메소드 삭제 , example2Leader() 메소드에 출력메세지 추가.

--- naverLoginSpring/src/main/java/com/itwill/naver/login/Example.java
+++ naverLoginSpring/src/main/java/com/itwill/naver/login/Example.java
@@ -12,11 +12,9 @@ public class Example {
 		System.out.println("github member 계정에서 작성한 메소드입니다.");
 
 	}
-	public void memberFunc() {
-		
-	}
+
 	public void example2Leader() {
-		
+		System.out.println("충돌 나나??????");
 	}
 	
 
